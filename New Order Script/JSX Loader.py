40f7aa9a3e0c5875d388f3947dd18b361e546e3d
TMichaelan <bigapplematthias@gmail.com>
2021-08-05 23:09:04 +0200

    adobe illustrator + process

--- New Order Script/JSX Loader.py
+++ New Order Script/JSX Loader.py
@@ -1,6 +1,8 @@
 import os, shutil, time
 
-PROCESS_PATH = 'C:\\Users\\bigap\\Desktop\\Print Postage'
+PROCESS_PATH = r'C:\Users\bigap\Desktop\Process'
+path_to_illustrator = r"C:\Program Files\Adobe\Adobe Illustrator CC 2019\Support Files\Contents\Windows"
+path_to_scripts = r"D:\PROJECTS\PYproj\Python Automation\Automating-File-Management-with-Python\New Order Script\JSX Scripts Illustrator"
 folder_list = ["2.5cm", "3.5cm", "4.5cm", "6.5cm", "9.5cm"]
 
 def execution():
@@ -16,19 +18,25 @@ def execution():
         for list_element in ls_of_folders:
             if os.path.isdir(PROCESS_PATH + "\\" + list_element):
                 list_of_folders.append(list_element)
+                ls_of_folders_1 = os.listdir(PROCESS_PATH + '\\' + list_element)
 
-        print(list_of_folders)
-        # # Going through the root folder
-        for folder in list_of_folders:
+                for list_element_1 in ls_of_folders_1:
 
-            file_list = os.listdir(PROCESS_PATH + "\\" + folder)
+                    path = PROCESS_PATH + '\\' + list_element + '\\' + list_element_1
 
-            for file_name in file_list:
+                    if os.path.isdir(path):
 
-                x = file_name + ' /c "JSX Scripts\\Batch Print PSD {folder}.jsx"'.format(folder=folder)
-                print(x)
-                # os.system(x)
+                        file_list = os.listdir(path)
 
+                        for file_name in file_list:
+                            os.system("cd {path_to_illustrator}".format(path_to_illustrator = path_to_illustrator))
+                            os.system(r"start Illustrator.exe {path}\{file_name}".format(path=path,file_name = file_name))
+                            time.sleep(10)
+                            if list_element in folder_list:
+                                jsx_script = "Batch Print {folder}.jsx".format(folder=list_element)
+                                os.system("\"{path_to_scripts}\{jsx_script}\"".format(path_to_scripts=path_to_scripts,jsx_script=jsx_script))
+                                time.sleep(5)
+                                os.system("taskkill /IM Illustrator.exe /F")
 
 
 start_time = time.time()
---
+++ New Order Script/test.py
@@ -0,0 +1,5 @@
+import os
+
+# os.system("cd / && D: && cd " + path_to_scripts + "\\" + jsx_script)
+# os.system(path_to_scripts + "\\" + jsx_script)
+# print( )
